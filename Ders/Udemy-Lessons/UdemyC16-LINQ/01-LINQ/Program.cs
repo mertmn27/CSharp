ad85b3127044f71d9c07ae9d2072eb3d8a69a435
Mert Osman Arslan <mertrsln1@gmail.com>
2026-03-16 14:16:12 +0300

    Eklendi

--- Ders/Udemy-Lessons/UdemyC16-LINQ/01-LINQ/Program.cs
+++ Ders/Udemy-Lessons/UdemyC16-LINQ/01-LINQ/Program.cs
@@ -6,7 +6,25 @@
         {
             dataSource ds = new dataSource();
             List<Musteri> musteriListe = ds.MusteriListesi();
-            Console.WriteLine(musteriListe.Count);
+
+            #region Alistirma
+
+            IEnumerable<Musteri> musteriListeAlistirma1 = musteriListe.Where(I => I.Ulke.StartsWith("A"));
+            int musteriAdetAlistirma1 = musteriListeAlistirma1.Count();
+
+
+            var musteriListeAlistirma2 = musteriListe.Where(I => I.Isim.Contains("b") && I.Ulke.Contains("a"));
+
+            var musteriListeAListirma3 = from I in musteriListe where I.DogumTarihi.Year > 1990 && I.Isim.Contains("a") select I;
+
+            var musteriListeAListirma3 = from I in musteriListe where I.DogumTarihi.Year > 1990 || I.Isim.Contains("a") select I;
+            #endregion
+
+            int toplamMusteriAdet1 = musteriListe.Where(I => I.Isim.StartsWith("A")).Count();
+
+            var toplamMusteriBulunan = from I in musteriListe where I.Isim.StartsWith("A") select I;
+
+            int toplamMusteriAdet2 = toplamMusteriBulunan.Count();
 
             int bulunanToplam = 0;
             for (int i = 0; i < musteriListe.Count; i++)
