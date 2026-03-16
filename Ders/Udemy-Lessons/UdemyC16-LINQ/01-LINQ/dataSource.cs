90fa0b186b63e05c1ee178d11942ad1633ea0eef
Mert Osman Arslan <mertrsln1@gmail.com>
2026-03-16 13:53:44 +0300

    Yenilendi

--- Ders/Udemy-Lessons/UdemyC16-LINQ/01-LINQ/dataSource.cs
+++ Ders/Udemy-Lessons/UdemyC16-LINQ/01-LINQ/dataSource.cs
@@ -21,6 +21,14 @@ namespace _01_LINQ
                 m.MusteriNumara = i++;
                 m.Isim = FakeData.NameData.GetFirstName();
                 m.Soyisim = FakeData.NameData.GetSurname();
+                m.DogumTarihi = FakeData.DateTimeData.GetDatetime(new DateTime(1984,01,01),new DateTime(1999,01,01));
+
+                m.Ulke = FakeData.PlaceData.GetCountry();
+                m.Il = FakeData.PlaceData.GetCity();
+                m.Ilce = FakeData.PlaceData.GetCountry();
+
+                m.Email = $"{m.Isim.ToLower()}.{m.Soyisim.ToLower()}@{FakeData.NetworkData.GetDomain()}";
+                m.TelefonNumarası = FakeData.PhoneNumberData.GetPhoneNumber();
 
                 musteriler.Add(m);
             }
